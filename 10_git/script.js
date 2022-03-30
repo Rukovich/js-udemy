c1c75f2d09d2fcb441a7eb1b2c259c9cd71c0b39
Stas <rukovich97@gmail.com>
2022-03-30 20:32:45 +0300

    first commit

---
+++ 10_git/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+&& - оператор И;
+\\ - оператор ИЛИ;
+!- оператор отрицания;
+*/
+const isChecked = true,
+  isClose = true;
+  console.log(isChecked && isClose);
+
---
+++ 9_Операторы в JS/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/*
+команды git
+  git config- конфигурация гита;
+  git status
+  git add -A- добавляет файлы в репозиторий;
+*/
+
