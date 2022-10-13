eb5f97bdeb622528f2d946f11f3addf3f0e2cb49
Stas <Rukovich97@gmail.com>
2022-10-13 21:01:00 +0300

    new course

--- 9_Операторы в JS/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*
-&& - оператор И;
-\\ - оператор ИЛИ;
-!- оператор отрицания;
-*/
-const isChecked = true,
-  isClose = true;
-  console.log(isChecked && isClose);
--- script.js
+++
@@ -1,6 +0,0 @@
-
-"use strict";
-
-
-const number0Films = +prompt ('сколько фильмов вы уже посмотрели?', '');
-alert (`вы посмотрели ${number0Films} фильмов`);
