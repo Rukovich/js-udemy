d007a9f8610f44079eefe0199fb7d25218be717f
Stas <Rukovich97@gmail.com>
2022-10-15 15:06:48 +0300

    lesson 10

--- JS_2022/10_Правила_и_типы_названия_переменных/script.js
+++ JS_2022/10_Правила_и_типы_названия_переменных/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// Как не стоит называть переменные:
+const a = 5000;
+const b = 4000;
+console.log('Ширина кузова автомобиля:' + a + ', длина' + b);
+
+// Как стоит:
+const vehicleBodyWidth = 5000;
+const vehicleBodylenght = 4000;
+console.log('Ширина кузова автомобиля:' + vehicleBodyWidth + ', длина' + vehicleBodylenght);
+
+/*
+Какие бывают стили написания:
+1. snake_case;
+2. UPPER_SNAKE_CASE- обычно так обозначают константы (COLOR_RED = '#F00';);
+3. _apiKey- такое тоже лучше не трогать;
+4. kebab-case- слова через "-";
+5. PascalCase- Первая буква большая;
+*/
