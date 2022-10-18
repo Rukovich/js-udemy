d604687a04e67da413e64da3597dcdc5ec1abc2c
Stas <Rukovich97@gmail.com>
2022-10-18 22:10:50 +0300

    update

--- JS_2022/11_Классификация_типов_данных/script.js
+++ JS_2022/11_Классификация_типов_данных/script.js
@@ -25,7 +25,29 @@ const obj = {
 };
 console.log(obj.name); //можем обращаться через "."
 console.log(obj['name']); //либо через "[]"
+/*
+Так же в объект можно добавить значение, с помощью "." или [].
+*/ 
+obj.weight = '60';
+obj['height'] = 160;
 
+console.log(obj.weight); //60
+console.log(obj.height); //160
+
+/*
+Внутри объектов возможно создавать вложенные структуры, например:
+*/ 
+const object = {
+  name: 'John',
+  age: 25,
+  isMarried: false,
+  children: {
+    name: 'Alex',
+      friend: {
+        name: 'Jack'
+      }
+  }
+};
 /*
 Рассмотрим массивы и как к ним обращаться
 */
