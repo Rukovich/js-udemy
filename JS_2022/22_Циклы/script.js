f689cb142d064a8bc73f78413c94e0269a21f11b
Stas <Rukovich97@gmail.com>
2022-10-26 22:31:49 +0300

    add tasks lesson 22

--- JS_2022/22_Циклы/script.js
+++ JS_2022/22_Циклы/script.js
@@ -5,27 +5,48 @@
 /*
 Цикл do-while
 */
-let num = 50;
+// let num = 50;
 
-while(num < 55) {
-  console.log(num);
-  num++;
-}
+// while(num < 55) {
+//   console.log(num);
+//   num++;
+// }
 
-do {
-  console.log(num);
-  num++; 
-}
-while (num < 55);
+// do {
+//   console.log(num);
+//   num++; 
+// }
+// while (num < 55);
 
 /*
 Цикл for
 */
 
-for (let i = 1; i < 10; i++) {
-  if (1 === 6) {
-    break; // - останавливает цикл
-    // continue; - позволяет пропустить ненужный шаг
-  }
-  console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//   if (1 === 6) {
+//     break; // - останавливает цикл
+//     // continue; - позволяет пропустить ненужный шаг
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//       continue;
+//   } else {
+//       console.log(i);
+//   }
+// }
+
+// let i = 2;
+// while (i <= 16) {
+//   if (i % 2 === 0) {
+//     i++;
+//     continue;
+// } else {
+//     console.log(i);
+// }
+// i++;
+// }
+// Место для пятой задачи
+
---
+++ JS_2022/22_Циклы/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let i = 2;
+while (i <= 16) {
+  if (i % 2 === 0) {
+    continue;
+} else {
+    console.log(i);
+}
+}
