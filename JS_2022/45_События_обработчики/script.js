47b979a2d3b2edaabda9fa95efe8557229ff8701
Stanislav <94227068+Rukovich@users.noreply.github.com>
2022-11-17 15:45:02 +0300

    lesson 45

--- JS_2022/45_События_обработчики/script.js
+++ JS_2022/45_События_обработчики/script.js
@@ -1,3 +1,34 @@
-/*
+// /*
 
-*/
+// */
+
+
+
+
+// //addEventListener() - Добавляем обработчик событий
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', () => {
+//     console.log('click')
+// })
+
+// //Удаляем элемент с помощью  event.target
+// //target - элемент на котором произошло событие
+// const btn = document.querySelector('button');
+
+// btn.addEventListener('click', (event) => {
+//     event.target.remove();
+// })
+
+
+// //Вызываем функцию по клику и удаляем элемент
+// const deleteEl = (el) => {
+//     el.target.remove();
+// }
+// btn.addEventListener('click', deleteEl);
+
+// // Назначаем обработчик событий на все элементы btns
+// const btns = document.querySelectorAll('button');
+// btns.forEach( btn => {
+//     btn.addEventListener('click', deleteEl);
+// })
