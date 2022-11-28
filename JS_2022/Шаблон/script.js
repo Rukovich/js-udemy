df863e4be2c261f8346ec6a69f75286f2148e880
Stas <Rukovich97@gmail.com>
2022-11-28 18:27:37 +0300

    chapter 2 lesson 1

--- JS_2022/Шаблон/script.js
+++ JS_2022/Шаблон/script.js
@@ -1,3 +1,33 @@
-/*
+// /*
 
-*/
+// */
+// function lovefunc(flower1, flower2) {
+//   if ((flower1 % 2 == 0 && flower2 % 2 == 1) || 
+//   (flower1 % 2 == 1 && flower2 % 2 == 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }   
+// }
+    
+// lovefunc(2, 5); //true
+// lovefunc(2, 4); //false
+
+
+const results = [2, 5, 5, 4, 3, 3, 5, 2];
+
+function betterThanAverage(classPoints, yourPoints) {
+  const sum = classPoints.reduce((total, amount) => total + amount);
+  const averageResult = sum / classPoints.length;
+
+  if (yourPoints > averageResult) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+}
+
+betterThanAverage(results, 4); //true
+betterThanAverage(results, 1); //false
---
+++ JS_Chapter_2/1_classlist/project/script.js
@@ -0,0 +1,43 @@
+/*
+
+*/
+const btns = document.querySelectorAll('button');
+const wrapper = document.querySelector('.btn-block');
+// console.log(btns[0].classList.length);
+
+/*
+Методы classList
+*/
+// console.log(btns[0].classList.item(0)); //находит элемент по списку
+// console.log(btns[1].classList.add('red', 'kakoitoClass')); //добавляет класс
+// console.log(btns[0].classList.remove('blue')); //Удаляет класс blue
+// console.log(btns[0].classList.toggle('blue')); //Переключатель. Удаляет / добавляет 
+
+//contains- проверяет наличие класса на определенном элемеенте и возвращает булиновое значение
+// if (btns[1].classList.contains('red')) {
+//   console.log('red');
+// }
+
+// btns[0].addEventListener('click', () => {
+  // if (!btns[1].classList.contains('red')) {
+  //   btns[1].classList.add('red');
+  // } else {
+  //   btns[1].classList.remove('red');
+  // }
+  ////Или можно так:
+  // btns[1].classList.toggle('red');
+// });
+
+/*
+Делегирование событий
+*/
+wrapper.addEventListener('click', (event) =>  {
+  if (event.target && event.target.tagName == "BUTTON") {
+    console.log('hello');
+  }
+});
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
+
+//
