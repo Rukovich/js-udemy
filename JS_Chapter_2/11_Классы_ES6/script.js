cc3f12208da7c04c5782efd40723c0579d389ccd
Stanislav <94227068+Rukovich@users.noreply.github.com>
2022-12-15 16:22:57 +0300

    lesson 11

--- JS_Chapter_2/11_Классы_ES6/script.js
+++ JS_Chapter_2/11_Классы_ES6/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+//Объявление классов
+// Классы имеют следующий синтаксис, например:
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+//Создаем метод внутри класса
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+//Теперь используем класс:
+const square = new Rectangle(10, 10);
+const long = new Rectangle(20, 100);
+
+console.log(square.calcArea()) //100
+console.log(long.calcArea()) //2000
+
+//Наследование
+//Например пропишем дополнительный класс, в котором будем создавать цветные прямоугольники. Данный класс будет наследовать все свойства предыдущего Rectangle
+//Для этого используем ключевое слово extends (наследуется от...)
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width); //вызывает значения родителя
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`)
+    }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red');
+
+div.showMyProps(); //Текст: Hello world, цвет: red
+console.log(div.calcArea()) //250
