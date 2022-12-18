0ddb6f4e418cba20662e3877cdfc5cfa96ff87f3
Stas <Rukovich97@gmail.com>
2022-12-18 17:25:20 +0300

    lesson 12

--- JS_Chapter_2/project/js/script.js
+++ JS_Chapter_2/project/js/script.js
@@ -1,137 +1,133 @@
-
-window.addEventListener('DOMContentLoaded', function() {
-
+window.addEventListener("DOMContentLoaded", function () {
   // Tabs
-  
-let tabs = document.querySelectorAll('.tabheader__item'),
-  tabsContent = document.querySelectorAll('.tabcontent'),
-  tabsParent = document.querySelector('.tabheader__items');
 
-function hideTabContent() {
-      
-      tabsContent.forEach(item => {
-          item.classList.add('hide');
-          item.classList.remove('show', 'fade');
-      });
+  let tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
 
-      tabs.forEach(item => {
-          item.classList.remove('tabheader__item_active');
-      });
-}
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
 
-function showTabContent(i = 0) {
-      tabsContent[i].classList.add('show', 'fade');
-      tabsContent[i].classList.remove('hide');
-      tabs[i].classList.add('tabheader__item_active');
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
   }
-  
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
   hideTabContent();
   showTabContent();
 
-tabsParent.addEventListener('click', function(event) {
-  const target = event.target;
-  if(target && target.classList.contains('tabheader__item')) {
-          tabs.forEach((item, i) => {
-              if (target == item) {
-                  hideTabContent();
-                  showTabContent(i);
-              }
-          });
-  }
+  tabsParent.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
   });
-  
+
   // Timer
 
-  const deadline = '2022-06-11';
+  const deadline = "2022-06-11";
 
   function getTimeRemaining(endtime) {
-      const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor( (t/(1000*60*60*24)) ),
-          seconds = Math.floor( (t/1000) % 60 ),
-          minutes = Math.floor( (t/1000/60) % 60 ),
-          hours = Math.floor( (t/(1000*60*60) % 24) );
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
 
-      return {
-          'total': t,
-          'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-      };
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
   }
 
-  function getZero(num){
-      if (num >= 0 && num < 10) { 
-          return '0' + num;
-      } else {
-          return num;
-      }
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
   }
 
   function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
 
-      const timer = document.querySelector(selector),
-          days = timer.querySelector("#days"),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(updateClock, 1000);
+    updateClock();
 
-      updateClock();
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
 
-      function updateClock() {
-          const t = getTimeRemaining(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
-          days.innerHTML = getZero(t.days);
-          hours.innerHTML = getZero(t.hours);
-          minutes.innerHTML = getZero(t.minutes);
-          seconds.innerHTML = getZero(t.seconds);
-
-          if (t.total <= 0) {
-              clearInterval(timeInterval);
-          }
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
       }
+    }
   }
 
-  setClock('.timer', deadline);
+  setClock(".timer", deadline);
 
   // Modal
 
-  const modalTrigger = document.querySelectorAll('[data-modal]');
-  const modal = document.querySelector('.modal');
-  const modalCloseBtn = document.querySelector('[data-close]');
+  const modalTrigger = document.querySelectorAll("[data-modal]");
+  const modal = document.querySelector(".modal");
+  const modalCloseBtn = document.querySelector("[data-close]");
 
   function openModal() {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
+    modal.classList.add("show");
+    modal.classList.remove("hide");
     // Либо вариант с toggle - но тогда назначить класс в верстке
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = "hidden";
     clearInterval(modaTimerId);
   }
-  
-  modalTrigger.forEach(btn => {
-      btn.addEventListener('click', openModal)
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", openModal);
   });
 
   function closeModal() {
-      modal.classList.add('hide');
-      modal.classList.remove('show');
-      // Либо вариант с toggle - но тогда назначить класс в верстке
-      document.body.style.overflow = '';
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    // Либо вариант с toggle - но тогда назначить класс в верстке
+    document.body.style.overflow = "";
   }
-  
-  modalCloseBtn.addEventListener('click', closeModal);
 
-  modal.addEventListener('click', (e) => {
-      if (e.target === modal) {
-          closeModal();
-      }
+  modalCloseBtn.addEventListener("click", closeModal);
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
   });
 
-  document.addEventListener('keydown', (e) => {
-      if (e.code === "Escape" && modal.classList.contains('show')) { 
-          closeModal();
-      }
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
   });
 
   // Модальное окно при прокрутке вниз страницы
@@ -139,15 +135,79 @@ tabsParent.addEventListener('click', function(event) {
   const modaTimerId = setTimeout(openModal, 3000);
 
   function showModalByScroll() {
-    if (window.pageYOffset + document.documentElement.clientHeight 
-      >= document.documentElement.scrollHeight -1) {
-        openModal();
-        window.removeEventListener('scroll', showModalByScroll);
-      }
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight - 1
+    ) {
+      openModal();
+      window.removeEventListener("scroll", showModalByScroll);
+    }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener("scroll", showModalByScroll);
 
   //Используем классы для карточек
+
+  class menuCard {
+    constructor(img, alt, subtitle, descr, price, parentSelector) {
+      this.img = img;
+      this.alt = alt;
+      this.subtitle = subtitle;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.currencyConverter();
+    }
+
+    currencyConverter() {
+      this.price = +this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+      element.innerHTML = `
+            <div class="menu__item">
+                <img src=${this.img} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
+      this.parent.append(element); //помещаем элемент в конец заданного узла
+    }
+  }
+
+  new menuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
+  new menuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    13,
+    '.menu .container'
+  ).render();
+
+  new menuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Фитнес"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков. ',
+    5,
+    '.menu .container'
+  ).render();
+
 
 });
