a0e6cae02ddc9c045c85f8d1c443019c50bd26ca
Stas <Rukovich97@gmail.com>
2022-12-18 17:55:06 +0300

    lesson 13

--- JS_Chapter_2/project/js/script.js
+++ JS_Chapter_2/project/js/script.js
@@ -149,12 +149,13 @@ window.addEventListener("DOMContentLoaded", function () {
   //Используем классы для карточек
 
   class menuCard {
-    constructor(img, alt, subtitle, descr, price, parentSelector) {
+    constructor(img, alt, subtitle, descr, price, parentSelector, ...classes) {
       this.img = img;
       this.alt = alt;
       this.subtitle = subtitle;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.currencyConverter();
@@ -166,16 +167,22 @@ window.addEventListener("DOMContentLoaded", function () {
 
     render() {
       const element = document.createElement("div");
+
+      if (this.classes.length === 0) { //задаем для класса параметры по умолчанию
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-            <div class="menu__item">
-                <img src=${this.img} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.subtitle}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
+            <img src=${this.img} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.subtitle}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>
         `;
       this.parent.append(element); //помещаем элемент в конец заданного узла
@@ -208,6 +215,4 @@ window.addEventListener("DOMContentLoaded", function () {
     5,
     '.menu .container'
   ).render();
-
-
 });
