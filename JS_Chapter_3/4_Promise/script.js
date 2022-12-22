fabc433c4a0e71084bc5f245483c5c4923b77fae
Stas <Rukovich97@gmail.com>
2022-12-22 16:03:48 +0300

    fetch API

--- JS_Chapter_3/4_Promise/script.js
+++ JS_Chapter_3/4_Promise/script.js
@@ -1,44 +1,44 @@
-'use strict';
+// 'use strict';
 
-//then() - 
-//catch() -
-//finally() -
+// //then() - 
+// //catch() -
+// //finally() -
 
-//Promise.all() - 
-//Promise.race() -
+// //Promise.all() - 
+// //Promise.race() -
 
-console.log('Запрос данных');
+// console.log('Запрос данных');
  
-const req = new Promise(function(resolve, reject) {
-  setTimeout(() => {
-    console.log('Подготовка данных...');
+// const req = new Promise(function(resolve, reject) {
+//   setTimeout(() => {
+//     console.log('Подготовка данных...');
 
-    const product = {
-      name: 'TV',
-      price: 2000
-    };
+//     const product = {
+//       name: 'TV',
+//       price: 2000
+//     };
 
-    //передаем данные
-    resolve(product);
-  }, 2000);
-});
+//     //передаем данные
+//     resolve(product);
+//   }, 2000);
+// });
 
-//Обрабатывает положительный результат
-req.then((product) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      product.status = 'order';
-      resolve(product);
-    }, 2000);
-  }).then(data => { //Используем промис
-    data.modify = true;
-    return data;
-  }).then((data) => {
-    console.log(data);
-  });
-}).catch(() => { //В случае ошибки
-  console.error('Произошла чудовищная ошибка');
-}).finally(() => { //В любом случае
-  console.log('Finally');
-});
+// //Обрабатывает положительный результат
+// req.then((product) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       product.status = 'order';
+//       resolve(product);
+//     }, 2000);
+//   }).then(data => { //Используем промис
+//     data.modify = true;
+//     return data;
+//   }).then((data) => {
+//     console.log(data);
+//   });
+// }).catch(() => { //В случае ошибки
+//   console.error('Произошла чудовищная ошибка');
+// }).finally(() => { //В любом случае
+//   console.log('Finally');
+// });
 
