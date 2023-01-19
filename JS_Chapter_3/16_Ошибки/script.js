1ced5ea848de3ae44ad06816bb7f683ba381c599
Stanislav <94227068+Rukovich@users.noreply.github.com>
2023-01-19 18:21:22 +0300

    add: lesson 16 errors

--- JS_Chapter_3/16_Ошибки/script.js
+++ JS_Chapter_3/16_Ошибки/script.js
@@ -1,2 +1,33 @@
 "use strict";
 
+const data = [
+  {
+      id: 'box',
+      tag: 'div'
+},
+  {
+      id: '',
+      tag: 'nav'
+  }
+]
+
+try {
+    data.forEach((blockObj, i) => {
+        const block = document.createElement(blockObj.tag);
+      
+        //Создаем свою ошибку
+        if (!blockObj.id) throw new Error('В данных под номером ${i + 1} нет id')
+      
+        block.setAttribute('id', blockObj.id);
+        document.body.append(block);
+      })
+} catch(e) {
+
+    if (e.name === 'syntaxError') { //добавляем условие
+        console.log(e.message);
+    //В ином случае выбрасываем ошибку и останавливаем код
+    } else throw e; 
+}
+
+// const err = new SyntaxError('bvhbuevhf');
+// console.log(err.name, err.message, err.stack);
