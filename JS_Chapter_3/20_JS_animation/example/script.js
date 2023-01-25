4791033636830d5991ba2724742af8e43be77ab8
rukovich97 <rukovich97@gmail.com>
2023-01-25 22:19:07 +0300

    add: lesson 20. animation 2

---
+++ JS_Chapter_3/20_JS_animation/example/script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector(".btn");
+const elem = document.querySelector(".box");
+let pos = 0;
+
+// Старая анимация
+
+// function myAnimation() {
+//     let pos = 0;
+
+//     const id = setInterval(frame, 10);
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + "px";
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
+
+function myAnimation() {
+  pos++;
+  elem.style.top = pos + "px";
+  elem.style.left = pos + "px";
+
+  if (pos < 300) {
+    requestAnimationFrame(myAnimation);
+  }
+}
+
+btn.addEventListener("click", () => requestAnimationFrame(myAnimation));
+
+let id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
